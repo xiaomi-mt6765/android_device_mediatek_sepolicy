63bf807fb9c1cb1d2c25cd74966f9102217bdbc5
zainarbani <zaintsyariev@gmail.com>
2019-08-11 10:47:54 +0100

    Add MTK sepolicy
    Signed-off-by: zainarbani <zaintsyariev@gmail.com>

---
+++ sepolicy.mk
@@ -0,0 +1,16 @@
+BOARD_SEPOLICY_DIRS += \
+    vendor/mediatek/sepolicy/basic/non_plat \
+    vendor/mediatek/sepolicy/bsp/non_plat \
+    vendor/mediatek/sepolicy/bsp/non_plat/ota
+
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/mediatek/sepolicy/basic/non_public \
+    vendor/mediatek/sepolicy/bsp/non_public \
+    vendor/mediatek/sepolicy/basic/prebuilts/api/26.0/plat_public \
+    vendor/mediatek/sepolicy/bsp/prebuilts/api/26.0/plat_public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/mediatek/sepolicy/basic/non_private \
+    vendor/mediatek/sepolicy/bsp/non_private \
+    vendor/mediatek/sepolicy/basic/prebuilts/api/26.0/plat_private \
+    vendor/mediatek/sepolicy/bsp/prebuilts/api/26.0/plat_private
